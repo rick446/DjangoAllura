2b10f28989807a5e521c0eff62751de395a2a192
Rick Copeland <rick@arborian.com>
2012-07-05 10:55:37 -0400

    Add custom tags for EW stuff
    Signed-off-by: Rick Copeland <rick@arborian.com>

--- djall/library.py
+++ djall/library.py
@@ -1,10 +1,45 @@
-import django.template
+from pylons import g, c
+from django import template
+from django.utils.safestring import mark_safe
 
-register = django.template.Library()
+register = template.Library()
+
+class EWRegionNode(template.Node):
+
+    def __init__(self, name):
+        self.name = name
+
+    def render(self, context):
+        result = list(g.resource_manager.emit(self.name))
+        return mark_safe(''.join(result))
+
+class DoNode(template.Node):
+
+    def __init__(self, statement):
+        self.statement = statement
+
+    def render(self, context):
+        exec self.statement in dict(context.dicts[-1], g=g, c=c)
+        return ''
 
 @register.tag
 def ewregion(parser, token):
-    return django.utils.safestring.mark_safe(
-        '<!-- This is an ew region -->')
+    contents = token.split_contents()
+    try:
+        tag, region = contents
+    except ValueError:
+        raise template.TemplateSyntaxError(
+            '%s requires a single argument' % contents[0])
+    return EWRegionNode(region)
+
+@register.tag
+def do(parser, token):
+    contents = token.split_contents()
+    try:
+        tag, statement = contents
+    except ValueError:
+        raise template.TemplateSyntaxError(
+            '%s requires a single argument' % contents[0])
+    return DoNode(statement)
 
 
